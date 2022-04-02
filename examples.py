545711ae71b0bd6fa8ec98e8bfcc8244f42dfffe
basavarajsannagoudar <basavarajsannagoudar777@gmail.com>
2022-04-02 14:46:06 +0530

    git setting up

--- examples.py
+++ examples.py
@@ -1,4 +1,28 @@
-hi this is a examples files
-updated from Web browser
-update from local system
-again update after the config setup
+ 113  git init
+  114  ls -lrth
+  115   ls -la
+  116  git status
+  117  git add
+  118  git add .
+  119  git commit -m "first commit"
+  120  git remote add origin https://github.com/basavarajsannagoudar/python.git
+  121  git push origin master
+  122  git commit
+  123  git clone
+  124  git clone https://github.com/basavarajsannagoudar/python.git
+  125  git status
+  126  git add
+  127  git add .
+  128  cd python/
+  129  clear
+  130  git status
+  131  git commit -a -m "up from local"
+  132   git config --global user.name "basavarajsannagoudar"
+  133   git config --global user.email "basavarajsannagoudar777@gmail.com"
+  134  git config --list
+  135  git status
+  136  git status
+  137  git commit -a -m "after config setting up"
+  138  git status
+  139  git push
+  140  history
